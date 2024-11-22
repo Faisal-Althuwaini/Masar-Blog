a92d935a6866562218e7f6294b3e1c8612087289
Faisal Al-Thuwaini <faisal.yalthuwaini@gmail.com>
2024-11-22 18:32:59 +0300

    uploading to render

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,7 @@ const entitiesPath = __dirname + '/**/*.entity{.ts,.js}';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.PORT) || 5432,
+      port: 5432,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
